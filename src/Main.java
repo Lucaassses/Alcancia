a292b3319364e90cdc9cdec79454c53704602f22
RPR-ZIPB204ES <RPR-ZIPB204ES@UMD.LOCAL.EDU>
2024-08-22 12:52:36 -0500

    Prueba2

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,73 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+//Lucas Rodriguez España
+
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        //Scanner scanner = new scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        int Mon20, Mon50, Mon100, Mon200, Mon500;
+
+        if (Mon20 == 20) {
+            Mon20++;
+
+        }else if(Mon50 == 50){
+            Mon50++;
+
+        }else if(Mon100 == 100){
+            Mon100++;
+
+        }else if(Mon200 == 200){
+            Mon200++;
+
+        }else if(Mon500 == 500){
+            Mon500++;
+        }
+
+
+        public int contarMoneda(int denominacion) {
+            int contador = 0;
+            switch (denominacion) {
+                case 20:
+                    contador = Mon20;
+                    break;
+                case 50:
+                    contador = Mon50;
+                    break;
+                case 100:
+                    contador = Mon100;
+                    break;
+                case 200:
+                    contador = Mon200;
+                    break;
+                case 500:
+                    contador = Mon500;
+                    break;
+            }
+
+            return contador;
+        }
+
+        public int calcularTotal(){
+            return Mon20 *20 + Mon50 *50 + Mon100 *100 + Mon200 *200 + Mon500 *500;
+        }
+        public void romperAlcancia(){
+
+            Mon20 = 0;
+            Mon50 = 0;
+            Mon100 = 0;
+            Mon200 = 0;
+            Mon500 = 0;
         }
     }
+
+
+
+
+
+
+}
+
+
 }
